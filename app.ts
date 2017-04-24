0c59476f5807629e60c817802b881067e5d99628
someone <wellschuan@gmail.com>
2017-04-24 15:39:23 +0800

    Added app.ts

---
+++ app.ts
@@ -0,0 +1,10 @@
+import * as http from "http";
+import * as url from "url";
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import errorHandler = require("errorhandler");
+import methodOverride = require("method-override");
+
+import * as routes from "./routes/index";
+
+var app = express();
