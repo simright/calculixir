ca93abc9856f5d5d33287d447c398ff0202fc12a
someone <wellschuan@gmail.com>
2017-04-24 16:07:53 +0800

    Updated app.ts => console.log(res&req)

--- app.js
+++ app.js
@@ -1,42 +1,28 @@
-import * as http from "http";
-import * as url from "url";
-import * as express from "express";
-import * as bodyParser from "body-parser";
-import errorHandler = require("errorhandler");
-import methodOverride = require("method-override");
-
-
+"use strict";
+exports.__esModule = true;
+var express = require("express");
+var bodyParser = require("body-parser");
+var errorHandler = require("errorhandler");
+var methodOverride = require("method-override");
 var app = express();
-
 // Application initialization 
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(methodOverride());
-
-
 // Environment
-
 var env = process.env.NODE_ENV || 'development';
 if (env === 'development') {
     app.use(errorHandler());
 }
-
-
 // Routes
-
-app.post('/', (req, res) => {
+app.post('/', function (req, res) {
     console.log("----------");
     console.log(req);
     console.log("==========");
     console.log(res);
-
-
 });
-
-
-app.listen(3000, function() {
+app.listen(3000, function () {
     console.log("It's running on port %d in %s mode", 3000, app.settings.env);
 });
-
-export var App = app;
+exports.App = app;
+//# sourceMappingURL=app.js.map
