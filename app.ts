9a5eda3c18397066cfabae056a7eb9af8f6283df
someone <wellschuan@gmail.com>
2017-04-24 15:53:15 +0800

    application schemed

--- app.ts
+++ app.ts
@@ -8,3 +8,29 @@ import methodOverride = require("method-override");
 import * as routes from "./routes/index";
 
 var app = express();
+
+// Application initialization 
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+
+// Environment
+
+var env = process.env.NODE_ENV || 'development';
+if (env === 'development') {
+    app.use(errorHandler());
+}
+
+
+// Routes
+
+app.post('/', route.index);
+
+
+app.listen(3000, function() {
+    console.log("It's running on port %d in %s mode", 3000, app.settings.env");
+});
+
+export var App = app;
