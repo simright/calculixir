20a173cb96573826ca84bafecc4a0b07ccbdb632
someone <wellschuan@gmail.com>
2017-04-27 12:30:19 +0800

    Added node.js example

--- examples/node/example.js
+++ examples/node/example.js
@@ -1,16 +1,19 @@
 var request = require('request')
-request({
 
-  // HTTP Archive Request Object
-  har: {
-    url: 'http://127.0.0.1:3000',
-    method: 'POST',
-    postData: {
-      'content-type': 'application/json',
-      body: {"data":"/app/data/demo/simple"}
-    }
-  }
-})
+request.post({
+     url: "http://127.0.0.1:3000",
+     headers: {
+        "Content-Type": "application/json"
+     },
+     body: {
+       "data": "/app/data/demo/simple"
+     },
+     json:true
+}, function(error, response, body){
+   console.log(error);
+   console.log(JSON.stringify(response['statusCode']));
+   console.log(body);
+});
 
 // a POST request will be sent to http://127.0.0.1:3000
 // with body an application/json body:
