fc354ca50543a1d50f0b68930ff332c6826aedc0
someone <wellschuan@gmail.com>
2017-04-27 12:37:12 +0800

    Renamed url & payload

--- examples/node/example.js
+++ examples/node/example.js
@@ -1,18 +1,30 @@
+/* author: Wells Jia */
+
 var request = require('request')
 
+
+const httpd_calculix_url = "http://127.0.0.1:3000";
+const payload = "/app/data/demo/simple";
+
+
 request.post({
-     url: "http://127.0.0.1:3000",
+     url: httpd_calculix_url,
      headers: {
         "Content-Type": "application/json"
      },
      body: {
-       "data": "/app/data/demo/simple"
+       "data": payload 
      },
      json:true
 }, function(error, response, body){
+   console.log("------------------------------------");
    console.log(error);
+   console.log("------------------------------------");
    console.log(JSON.stringify(response['statusCode']));
+   console.log("------------------------------------");
    console.log(body);
+   console.log("------------------------------------");
+
 });
 
 // a POST request will be sent to http://127.0.0.1:3000
